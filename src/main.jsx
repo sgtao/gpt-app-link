3784a91a7043e4b237c779dc59ae52d564257aab
Shogo Ogami <sg.tao.so@gmail.com>
2023-11-08 17:42:40 +0900

    update extension for change popup to content_scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
+// for contents.js
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.createElement('div');
+root.id = 'root-gpt-app-links';
+document.body.appendChild(root);
+
+ReactDOM.createRoot(document.getElementById('root-gpt-app-links')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
